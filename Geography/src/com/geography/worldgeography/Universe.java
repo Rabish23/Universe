b6101119b31e82bf96f631e81db5e4d374a6748f
Rabish23 <rabishkumar2323@gmail.com>
2021-04-16 07:53:44 +0530

    universe

---
+++ Geography/src/com/geography/worldgeography/Universe.java
@@ -0,0 +1,18 @@
+package com.geography.worldgeography;
+
+public class Universe {
+	public static void main(String[] args) {
+		Universe uni=new Universe();
+		uni.galaxy();
+	}
+	public void galaxy() {
+		System.out.println("Our Galaxy: Milky Way");
+		System.out.println("Nearest Galaxy : Andromeda");
+		System.out.println("Latest Known Galaxy: Dwarf Galaxy");
+		System.out.println("Neutron Star:- star having diameter of 20kmand mass is 1.4times greater than sun");
+		System.out.println("Brightest Star outside solar system:- Sirius/dogstar");
+		System.out.println("Proxima centuria is nearest star to our solar system after that alpha centuria after that barnard star ");
+		System.out.println("Black Hole:- when the stars die");
+		System.out.println("chandrashekhar limit:-upper limit of any star can be 1.44 time the mass of sun");
+	}
+}
