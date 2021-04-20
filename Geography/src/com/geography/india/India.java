905c868d2e5e78bb2fe12c8dc69fc22e0aea5cda
Rabish23 <rabishkumar2323@gmail.com>
2021-04-20 06:31:29 +0530

    random add

--- Geography/src/com/geography/india/India.java
+++ Geography/src/com/geography/india/India.java
@@ -3,6 +3,7 @@ package com.geography.india;
 public class India {
 	public static void main(String[] args) {
 		System.out.println("i love my india");
+		System.out.println("India has diverse culture");
 	}
 
 }
