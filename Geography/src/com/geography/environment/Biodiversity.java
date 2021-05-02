9b69928cab8c11256c30d2e0bad2a7650a0f27fb
Rabish23 <rabishkumar2323@gmail.com>
2021-05-02 19:13:19 +0530

    started work in git branch ASE

---
+++ Geography/src/com/geography/environment/Biodiversity.java
@@ -0,0 +1,11 @@
+package com.geography.environment;
+
+public class Biodiversity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Introducing the biodiversity");
+
+	}
+
+}
